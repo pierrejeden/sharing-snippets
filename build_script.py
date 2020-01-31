fdcb21322e53d11a33af64b8883af9eeb2297102
pierrejeden <42551105+pierrejeden@users.noreply.github.com>
2020-01-31 22:04:47 +0100

    scrap

--- build_script.py
+++
@@ -1,8 +0,0 @@
-import docker
-import python_pachyderm as pach
-
-docli = docker.from_env()
-pacli = pach.Client()
-
-docli.images.build(path='pipelines', dockerfile='Dockerfile_a', tag='aaa-image')
-docli.images.build(path='pipelines', dockerfile='Dockerfile_b', tag='bbb-image')
